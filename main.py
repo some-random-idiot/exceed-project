0e0d7bc72cee52cf78fdab9527ed224b8f296b7b
some random idiot <sittipat2554aa@hotmail.com>
2022-02-16 00:34:52 +0700

    Added a POST request method for updating boat status.

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ client = MongoClient("localhost", 27017)
 database = client["project"]
 boat_status_collection = database["boat_status"]
 schedule_collection = database["schedule"]
+
+
+class BoatStatus(BaseModel):
+    where: int  # 0: start, 1: end
+    passed: int  # 1: laser 1, 2: laser 2
+
+
+@app.post("/update-boat-status")
+def update_boat_status(boat_status: BoatStatus):
+    boat_status_collection.insert_one(boat_status.dict())
+    return {"status": "Boat status updated!"}
