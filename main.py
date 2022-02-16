37a35b9574bcc9e9f37faef589687f07c7db0a03
some random idiot <sittipat2554aa@hotmail.com>
2022-02-16 08:10:53 +0700

    Made it so that the boat's status is stored in variables, instead of the database.

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ schedule_collection = database["schedule"]
 estimate_collection = database["estimate"]
 
 
+# Store boat status in variables for quicker access.
+where: int
+passed: int
+
+
 class BoatStatus(BaseModel):
     where: int = Field(..., gt=-1, lt=2)  # 0: start, 1: end
     passed: int = Field(..., gt=0, lt=3)  # 1: laser 1, 2: laser 2
@@ -26,16 +31,18 @@ class Schedule(BaseModel):
 
 @app.get("/get-status")
 def get_boat_status():
-    return boat_status_collection.find_one({}, {"_id": 0})
+    return {"where": where,
+            "passed": passed}
 
 
 @app.post("/update-status")
 def update_boat_status(boat_status: BoatStatus):
-    boat_status = boat_status.dict()
-    boat_status_collection.update_one({}, {"$set": boat_status}, upsert=True)
+    global where, passed
+    where = boat_status.where
+    passed = boat_status.passed
     return {"status": "Boat status updated!",
-            "where": boat_status["where"],
-            "passed": boat_status["passed"]}
+            "where": where,
+            "passed": passed}
 
 
 @app.get("/get-schedule")
