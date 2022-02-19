f8b8543481202e9f46a1e1da444e087884a3ea14
some random idiot <sittipat2554aa@hotmail.com>
2022-02-19 15:07:49 +0700

    Added a request body error handling to edit_schedule.

--- main.py
+++ main.py
@@ -134,19 +134,23 @@ def create_schedule(schedule: Schedule):
 async def edit_schedule(schedule: Request):
     """Delete the old schedule and create a new one. If the new schedule information already exists, return an error."""
     schedule = await schedule.json()
-    if schedule_collection.find_one({"day_name": schedule["old_day_name"], "time": schedule["old_time"]}) is None:
-        return {"status": "The schedule targeted for change does not exist!"}
-    if schedule_collection.find_one({"day_name": schedule["day_name"], "time": schedule["time"]}) is not None:
-        return {"status": "The provided new schedule combination already exists!"}
-    else:
-        new_schedule = {"day_name": schedule["day_name"], "time": schedule["time"]}
-        schedule_collection.delete_one({"day_name": schedule["old_day_name"], "time": schedule["old_time"]})
-        schedule_collection.insert_one(new_schedule)
-        return {"status": "Schedule edited!",
-                "day_name": schedule["day_name"],
-                "time": schedule["time"],
-                "old_day_name": schedule["old_day_name"],
-                "old_time": schedule["old_time"]}
+
+    try:
+        if schedule_collection.find_one({"day_name": schedule["old_day_name"], "time": schedule["old_time"]}) is None:
+            return {"status": "The schedule targeted for change does not exist!"}
+        if schedule_collection.find_one({"day_name": schedule["day_name"], "time": schedule["time"]}) is not None:
+            return {"status": "The provided new schedule combination already exists!"}
+        else:
+            new_schedule = {"day_name": schedule["day_name"], "time": schedule["time"]}
+            schedule_collection.delete_one({"day_name": schedule["old_day_name"], "time": schedule["old_time"]})
+            schedule_collection.insert_one(new_schedule)
+            return {"status": "Schedule edited!",
+                    "day_name": schedule["day_name"],
+                    "time": schedule["time"],
+                    "old_day_name": schedule["old_day_name"],
+                    "old_time": schedule["old_time"]}
+    except KeyError:
+        return {"status": "Unexpected request body!"}
 
 
 @app.delete("/delete-schedule")
